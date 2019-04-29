2c8a97e2b838f0e473d1791071653185753c57a8
mussabaheen@gmail.com <mussabaheen@gmail.com>
2019-04-29 21:57:14 +0500

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -10,8 +10,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
+
       title: "Calculator App",
-      theme: ThemeData.dark(),
+      theme:ThemeData.dark(),
       home: new homepage(),
 
 
